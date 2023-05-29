3a76565bdfb788699232ebb595e17ffbc02c27bd
Shai <shai>
2023-05-29 11:49:18 +0800

    Added change B

---
+++ P00_C206/src/HelloWorld.java
@@ -0,0 +1,23 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * abila, 29 May 2023 9:21:52 am
+ */
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My First Java Program");
+		
+		//change A
+		//Change B
+	}
+
+}
